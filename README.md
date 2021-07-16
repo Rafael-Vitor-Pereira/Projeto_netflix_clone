e89fab18b9b9dc0cbf99f6fd7b13a74c4f65b7d4
Rafael-Vitor-Pereira <rafaelpereira0599@gmail.com>
2021-07-16 13:14:45 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Projeto_netflix_clone
+
+
+
+##### Clone da interface da netflix as funcionalidades da página ainda não foram adicionadas, as mesmas serão adicionadas posteriormente.
+
+
+
---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:2.2
+        },
+        600:{
+            items:4.2
+        },
+        1000:{
+            items:6.2
+        }
+    }
+});
